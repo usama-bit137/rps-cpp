d2593741c48aecffa2864021128d3d506b647fb1
usama-bit137 <usamaaqeel98@gmail.com>
2024-04-04 23:02:30 +0100

    Win/Lose logic implemented.

--- rps.cpp
+++ rps.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std; 
 
 string userInputController();
 string choiceChecker(string choice);
 
 string choiceChecker(string choiceInput) {
-  // THIS WORKS ✅
   string choiceString;
+  
   if (choiceInput=="r") {
     choiceString = "rock";
   } else if (choiceInput == "p") {
@@ -16,23 +17,48 @@ string choiceChecker(string choiceInput) {
   } else {
     choiceString = userInputController();
   }
+  
   return choiceString;
 }
 
 string userInputController() {
-  // THIS ALSO WORKS ✅
   string choiceInput; 
+  
   while(choiceInput != "r" && choiceInput != "p" && choiceInput != "s" ) {  
     cout << "Pick - r (rock), p (paper) or s (scissors): ";
     cin >> choiceInput;
   }
-
+  
   return choiceInput;
 }
 
+string winLoseLogic(string userChoice, string computerChoice) {
+  string verdict;
+  if(userChoice == computerChoice) {
+    verdict = "DRAW!!!";
+  } else if (userChoice == "rock") {
+    verdict = (computerChoice == "scissors") ? "You win :)" : "Computer Wins :(";
+  } else if (userChoice == "paper") {
+    verdict = (computerChoice == "rock") ? "You Win :)" : "Computer Wins :(";
+  } else if (userChoice == "scissors") {
+    verdict = (computerChoice == "paper") ? "You Win :)" : "Computer Wins :(";
+  }
+  return verdict;
+}
+
 int main() {
-  string choice = userInputController();
-  choice = choiceChecker(choice);
+  // Sanitize user choice and checking: 
+  string userChoice = userInputController();
+  userChoice = choiceChecker(userChoice);
+  cout << "You Chose: "<< userChoice << endl;
+
+  // Computer choice: 
+  // Random Package:
+  int intComputerChoice = rand() % 3;
+  string computerChoices[3] = {"r", "p", "s"};
   
-  cout << choice << endl;
+  string stringComputerChoice = choiceChecker(computerChoices[intComputerChoice]);
+  cout << "Computer Chose: " << stringComputerChoice << endl;
+
+  cout << winLoseLogic(userChoice, stringComputerChoice);
 }
